a83591a9aa6b9b7845261a5e717eadb9b024b964
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-08-04 19:55:21 +0100

    For loop practice without sum & len function
    Calculate avg height of students

---
+++ AvgHeightChecker.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+#Calculate avg without sum() & len() function
+
+print(student_heights)
+total_height = 0
+total_students = 0
+
+for height in student_heights:
+  total_height += height
+  total_students +=1
+
+avg_height = total_height / total_students
+
+print(round(avg_height))
+
+
+
+  
+
